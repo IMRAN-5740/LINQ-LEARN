1293a577adc3e20ecc4910d25b16b0f6d2d28ac6
imran <imranbsmrstucse@gmail.com>
2023-01-24 23:46:16 +0600

    New File Added

---
+++ LINQ/LINQ.Samples1/LINQ.Samples1/Program.cs
@@ -0,0 +1,22 @@
+﻿
+//Query Syntax:
+
+List<int> list = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 };
+var querySyntax = from obj in list
+                  where obj > 5
+                  select obj;
+
+foreach (var item in querySyntax)
+{
+    Console.WriteLine(item);
+}
+
+
+//Method Syntax:
+
+var methodSyntax=list.Where(x => x > 5);
+
+foreach (var item in methodSyntax)
+{
+    Console.WriteLine(item);
+}
