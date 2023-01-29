35579f8b946b2281784427087b4e1ddebfaf8ad1
imran <imranbsmrstucse@gmail.com>
2023-01-29 17:10:40 +0600

    Pagining Implement using Skip and Take Method

---
+++ LINQ/LINQ.Samples1/PaginginLINQ/Program.cs
@@ -0,0 +1,28 @@
+﻿
+using PaginginLINQ;
+int totalPageView = 5;
+do
+{
+    
+    Console.WriteLine("Enter page number");
+    if (int.TryParse(Console.ReadLine(), out int pageNumber))
+    {
+        if((pageNumber*totalPageView)>SchoolEmployee.GetSchoolEmployees().Count())
+        {
+            Console.WriteLine("Out of Range Please enter a valid page number");
+        }    
+        else
+        { 
+           var methodSyntax = SchoolEmployee.GetSchoolEmployees().Skip((pageNumber - 1) * totalPageView).Take(totalPageView).ToList();
+            foreach (var employee in methodSyntax)
+            {
+                Console.WriteLine($"Employee ID : {employee.EmployeeId}  Employee Name : {employee.EmployeeName}  Employee Email : {employee.Email}");
+            }
+        }
+    }
+    else
+    {
+        Console.WriteLine("Please Enter a valid Page number");
+    } 
+} while (true);
+
