76d9124a46f5e8b802fb33a77f0790aeb2207671
imran <imranbsmrstucse@gmail.com>
2023-01-30 23:42:38 +0600

    Join in Linq

---
+++ LINQ/LINQ.Samples1/JoiningInLINQ/Program.cs
@@ -0,0 +1,42 @@
+﻿using JoiningInLINQ;
+
+List<Subject>subjects=new List<Subject>()
+{
+     new Subject(){SubjectId=1,SubjectName="CSE",StudentID=1},
+     new Subject(){SubjectId=2,SubjectName="EEE",StudentID=2},
+     new Subject(){SubjectId=3,SubjectName="ACCE",StudentID=0},
+     new Subject(){SubjectId=4,SubjectName="CE",StudentID=1},
+     new Subject(){SubjectId=5,SubjectName="FAE",StudentID=2},
+     new Subject(){SubjectId=6,SubjectName="ARC",StudentID=3}
+};
+
+List<Student> students = new List<Student>()
+{
+    new Student(){Id=1,StudentName="Mohammad Imran" ,StudentEmail="imran@gmail.com"},
+    new Student(){Id=2,StudentName="A " ,StudentEmail="a@gmail.com"},
+    new Student(){Id=3,StudentName="B " ,StudentEmail="b@gmail.com"},
+    new Student(){Id=4,StudentName="C " ,StudentEmail="c@gmail.com"},
+    new Student(){Id=5,StudentName="D " ,StudentEmail="d@gmail.com"},
+    new Student(){Id=6,StudentName="E " ,StudentEmail="e@gmail.com"}
+};
+
+
+var querySyntax=(from sub in subjects 
+                join std in students
+                on sub.StudentID equals std.Id
+                select new
+                {
+                    StudentName=std.StudentName,
+                    StudentEmail=std.StudentEmail,
+                    SubjectId = sub.SubjectId,
+                    SubjectName =sub.SubjectName
+                }).ToList();
+
+
+
+
+foreach(var s in querySyntax)
+{
+    Console.WriteLine($"Student Name :{s.StudentName} StudentEmail :{s.StudentEmail} SubjectId :{s.SubjectId} SubjectName :{s.SubjectName}");
+}
+Console.WriteLine();
--- LINQ/LINQ.Samples1/PaginginLINQ/SchoolEmployee.cs
+++ LINQ/LINQ.Samples1/PaginginLINQ/SchoolEmployee.cs
@@ -14,29 +14,29 @@ namespace PaginginLINQ
         public static List<SchoolEmployee> GetSchoolEmployees()
         {
             return new List<SchoolEmployee>()
-        {
+                {
 
-            new SchoolEmployee(){EmployeeId=1,EmployeeName="Mohamamd Imran",Email="imranbsmrstucse@gmail.com"},
-            new SchoolEmployee(){EmployeeId=2,EmployeeName="A Imran",Email="a@gmail.com"},
-            new SchoolEmployee(){EmployeeId=3,EmployeeName="B Imran",Email="b@gmail.com"},
-            new SchoolEmployee(){EmployeeId=4,EmployeeName="C Imran",Email="v@gmail.com"},
-            new SchoolEmployee(){EmployeeId=5,EmployeeName="D Imran",Email="d@gmail.com"},
-            new SchoolEmployee(){EmployeeId=6,EmployeeName="E Imran",Email="e@gmail.com"},
-            new SchoolEmployee(){EmployeeId=7,EmployeeName="F Imran",Email="f@gmail.com"},
-            new SchoolEmployee(){EmployeeId=8,EmployeeName="G Imran",Email="g@gmail.com"},
-            new SchoolEmployee(){EmployeeId=9,EmployeeName="H Imran",Email="h@gmail.com"},
-            new SchoolEmployee(){EmployeeId=10,EmployeeName="I Imran",Email="i@gmail.com"},
-            new SchoolEmployee(){EmployeeId=11,EmployeeName="J Imran",Email="j@gmail.com"},
-            new SchoolEmployee(){EmployeeId=12,EmployeeName="K Imran",Email="k@gmail.com"},
-            new SchoolEmployee(){EmployeeId=13,EmployeeName="L Imran",Email="l@gmail.com"},
-            new SchoolEmployee(){EmployeeId=14,EmployeeName="M Imran",Email="m@gmail.com"},
-            new SchoolEmployee(){EmployeeId=15,EmployeeName="N Imran",Email="n@gmail.com"},
-            new SchoolEmployee(){EmployeeId=16,EmployeeName="O Imran",Email="o@gmail.com"},
-            new SchoolEmployee(){EmployeeId=17,EmployeeName="P Imran",Email="p@gmail.com"},
-            new SchoolEmployee(){EmployeeId=18,EmployeeName="Q Imran",Email="q@gmail.com"},
-            new SchoolEmployee(){EmployeeId=19,EmployeeName="R Imran",Email="r@gmail.com"},
-            new SchoolEmployee(){EmployeeId=20,EmployeeName="S Imran",Email="s@gmail.com"}
-        };
+                    new SchoolEmployee(){EmployeeId=1,EmployeeName="Mohamamd Imran",Email="imranbsmrstucse@gmail.com"},
+                    new SchoolEmployee(){EmployeeId=2,EmployeeName="A Imran",Email="a@gmail.com"},
+                    new SchoolEmployee(){EmployeeId=3,EmployeeName="B Imran",Email="b@gmail.com"},
+                    new SchoolEmployee(){EmployeeId=4,EmployeeName="C Imran",Email="v@gmail.com"},
+                    new SchoolEmployee(){EmployeeId=5,EmployeeName="D Imran",Email="d@gmail.com"},
+                    new SchoolEmployee(){EmployeeId=6,EmployeeName="E Imran",Email="e@gmail.com"},
+                    new SchoolEmployee(){EmployeeId=7,EmployeeName="F Imran",Email="f@gmail.com"},
+                    new SchoolEmployee(){EmployeeId=8,EmployeeName="G Imran",Email="g@gmail.com"},
+                    new SchoolEmployee(){EmployeeId=9,EmployeeName="H Imran",Email="h@gmail.com"},
+                    new SchoolEmployee(){EmployeeId=10,EmployeeName="I Imran",Email="i@gmail.com"},
+                    new SchoolEmployee(){EmployeeId=11,EmployeeName="J Imran",Email="j@gmail.com"},
+                    new SchoolEmployee(){EmployeeId=12,EmployeeName="K Imran",Email="k@gmail.com"},
+                    new SchoolEmployee(){EmployeeId=13,EmployeeName="L Imran",Email="l@gmail.com"},
+                    new SchoolEmployee(){EmployeeId=14,EmployeeName="M Imran",Email="m@gmail.com"},
+                    new SchoolEmployee(){EmployeeId=15,EmployeeName="N Imran",Email="n@gmail.com"},
+                    new SchoolEmployee(){EmployeeId=16,EmployeeName="O Imran",Email="o@gmail.com"},
+                    new SchoolEmployee(){EmployeeId=17,EmployeeName="P Imran",Email="p@gmail.com"},
+                    new SchoolEmployee(){EmployeeId=18,EmployeeName="Q Imran",Email="q@gmail.com"},
+                    new SchoolEmployee(){EmployeeId=19,EmployeeName="R Imran",Email="r@gmail.com"},
+                    new SchoolEmployee(){EmployeeId=20,EmployeeName="S Imran",Email="s@gmail.com"}
+                };
         }
 
     }
