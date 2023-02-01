45021919e0ff9258d21b56e7ef2c0f4279f8f81e
imran <imranbsmrstucse@gmail.com>
2023-02-01 16:39:45 +0600

    Element and ElementOrDefault

---
+++ LINQ/LINQ.Samples1/ElementOperationsinLINQ/Program.cs
@@ -0,0 +1,11 @@
+﻿List<int> numbers = new List<int>() { 1,2,3,4,5,6,67,78};
+//var methodSyntax = numbers.ElementAt(10);
+var methodSyntax = numbers.ElementAtOrDefault(10);
+
+//var querySyntax=(from num in numbers
+ //               select num ).ElementAt(10);
+var querySyntax = (from num in numbers
+                   select num).ElementAtOrDefault(10);
+
+
+Console.ReadLine();
